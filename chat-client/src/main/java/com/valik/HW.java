42074dc14624d2660d9024c2860725f895ceac68
nortrix <nortrix14@gmail.com>
2014-08-23 17:57:49 +0300

    add a client

---
+++ chat-client/src/main/java/com/valik/HW.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.valik;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+
+/**
+ *
+ * @author Валик
+ */
+public class HW {
+    public static void main(String[] args) throws IOException {
+       URL localhost = new URL("http://localhost:4567/hello");
+        URLConnection yc = localhost.openConnection();
+        BufferedReader in = new BufferedReader(
+                                new InputStreamReader(
+                                yc.getInputStream()));
+        String inputLine;
+
+        while ((inputLine = in.readLine()) != null) 
+            System.out.println(inputLine);
+        in.close();
+    }
+}
